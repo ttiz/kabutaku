7900ebf6af9900dd67930530ef27ee0667d4a551
tatsu <tatsukichisan@protonmail.com>
2023-11-20 00:03:58 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
+  <div id="app">
+    <input type="number" v-model.number="price">
+    <p>{{ price }}</p>
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      price: '',
+    }
+  },
+}
+</script>
